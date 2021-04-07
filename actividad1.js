33a5327886a75f72814bdb4eadb3892dfb633590
JUAN ARANGO <69176004+juandiego19578@users.noreply.github.com>
2021-04-07 17:27:58 -0500

    ejercicio1

---
+++ actividad1.js
@@ -0,0 +1,11 @@
+/**1. **Escribe un programa de una sola línea que escriba en la pantalla un texto que
+ diga “Hello World”. console.log**
+
+git add . 
+git commit - 'ejercico 1'
+
+git push origin <nombre de la rama>
+**/
+
+console.log("Hello World")
+console.log("verificando primer ejercicio")
